385101794ea8d218bdaec864d73f4b7801e891c8
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-06-18 11:35:51 -0300

    add insert last/first and print data function

--- script.js
+++ script.js
@@ -5,5 +5,59 @@ class Node {
     } 
 }
 
-const n1 = new Node(100);
-console.log(n1);
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    // insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size += 1;
+    }
+    
+    // insert last node
+     insertLast(data){
+        let node = new Node(data); 
+        let current 
+
+    // if empty make head 
+    if (!this.head) {
+        this.head = node;
+    } else {
+        current = this.head;
+
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+        }
+        this.size += 1;
+    }
+    // insert at index
+
+    // get at index 
+
+    // remove at index
+
+    // clear list
+
+    // print list data
+    printListData() {
+        let current = this.head;
+
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const ll = new LinkedList();
+
+console.log(ll)
+
+ll.insertLast(400)
+ll.insertFirst(100)
+ll.insertFirst(150)
+ll.printListData()
