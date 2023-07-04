8787c21d8c8eeb91af3467f4a2742ceb68fb4dbc
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-07-03 22:58:46 -0300

    add new gameboard function

--- knights-travails/gameboard.js
+++ knights-travails/gameboard.js
@@ -1,51 +1,13 @@
-// Create chessboard 
-const gameBoard = () => {
-    // Initiate variables and create chessboard table
-    const defaultStartLocation = [4,5];
-    const coordArray = [];
-    const chessTable = document.createElement("table");
-
-    // Create the board cells and apply position values to each cell
-    chessTable.setAttribute("class", "center");
-    for(let i = 0; i < 8; i += 1) {
-        const tableRow = document.createElement("tr");
-        let cellRowCoord = Math.abs(i - 7);
-        tableRow.textContent = cellRowCoord; 
-        for (let z = 0; z < 8; z += 1) {
-            const tableCell = document.createElement("td");
-            let cellColumnCoord = z;
-            tableCell.textContent = cellColumnCoord;
-            // Loop to change color of cell of odd values 
-            if((i + z) % 2 == 0) {
-                tableCell.setAttribute("class", "cell whitecell");
-                tableRow.appendChild(tableCell);
-                coordArray.push(cellRowCoord);
-                coordArray.push(cellColumnCoord);
-                tableCell.dataset.coordArray = coordArray;
-                coordArray.splice(0,2);
-            } else {
-                tableCell.setAttribute("class", "cell blackcell");
-                tableRow.appendChild(tableCell);
-                coordArray.push(cellRowCoord);
-                coordArray.push(cellColumnCoord);
-                tableCell.dataset.coordArray = coordArray;
-                coordArray.splice(0,2);
-            }
+// Create board
+const gameBoard = (x,y) => {
+    let boardCoordinates = [];
+    for (let i = 0; i < x; i += 1) {
+        for (let j = 0; j < y; j += 1) {
+            boardCoordinates.push([i,j]);
         }
-    chessTable.appendChild(tableRow);    
     }
-
-    // Create Knight and position him at 0,0
-    const cellNodes = chessTable.querySelectorAll("td");
-    cellNodes.forEach((cellNode) => {
-        if(defaultStartLocation.toString() === cellNode.dataset.coordArray) {
-            let knightImg = document.createElement("img");
-            knightImg.src = "./assets/knight.svg";
-            cellNode.appendChild(knightImg);
-        }
-    });
-    document.body.appendChild(chessTable)
+    console.log(boardCoordinates);
+    return boardCoordinates;
 }
 
-export {gameBoard};
-
+gameBoard(8,8)
